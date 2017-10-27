86154a4fbb7646457f5a73f2206e7868ca45d2e0
Guilherme Budnieswski <budnieswski@gmail.com>
2017-10-27 20:28:58 -0200

    adsadadasds

--- db.php
+++ db.php
@@ -18,7 +18,7 @@ foreach ($_SERVER as $key => $value) {
 
 $con = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
-if (!$link) {
+if (!$con) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
@@ -26,9 +26,9 @@ if (!$link) {
 }
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($con) . PHP_EOL;
 
-mysqli_close($link);
+mysqli_close($con);
 
 // die("On DB File");
 
