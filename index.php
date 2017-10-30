437bb76a02a5a3152a126b641cac13504d5259ae
Guilherme Budnieswski <budnieswski@gmail.com>
2017-10-30 14:28:35 -0200

    Log navigation

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+
+include('db.php');
+
+$query = "INSERT into `navigation` (agent, server) VALUES ('".$_SERVER['HTTP_USER_AGENT']."', '".json_encode($_SERVER)."')";
+$result = mysqli_query($con,$query);
+if($result){
+    echo $_SERVER['HTTP_USER_AGENT'];
+} else {
+  echo "An error";
+}
+
+
+
+die();
 include("auth.php"); //include auth.php file on all secure pages ?>
 <!DOCTYPE html>
 <html>
